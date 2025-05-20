4db59b6b36eb690d0c3bb1ff1d4658024c52c77a
sirius000098 <33917463+sirius000098@users.noreply.github.com>
2025-05-20 21:56:10 +0800

    release 20250520

--- hello_world.py
+++ hello_world.py
@@ -1,15 +1,15 @@
-import random
+#import random
 
 # 创建包含 "hello world" 和其他 7 个随机数的字典
 data = {
     "message": "hello world",
-    "number1": random.randint(1, 100),
-    "number2": random.randint(1, 100),
-    "number3": random.randint(1, 100),
-    "number4": random.randint(1, 100),
-    "number5": random.randint(1, 100),
-    "number6": random.randint(1, 100),
-    "number7": random.randint(1, 100)
+#     "number1": random.randint(1, 100),
+#     "number2": random.randint(1, 100),
+#     "number3": random.randint(1, 100),
+#     "number4": random.randint(1, 100),
+#     "number5": random.randint(1, 100),
+#     "number6": random.randint(1, 100),
+#     "number7": random.randint(1, 100)
 }
 
 # 打印字典内容
